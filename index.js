68671d8845367aff8f176a8a92e282f86f6a72b1
KarthikPaidipalli <paidipallikarthik1510@gmail.com>
2024-05-21 10:54:39 +0530

    lowercase is added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function find() {
     alert("Some times spaces may include")
-    let originalText = document.getElementById("orginal").value.toLowerCase().split(/\s+/);
-    let changedText = document.getElementById("changed").value.toLowerCase().split(/\s+/);
+    let originalText = document.getElementById("orginal").value.split(/\s+/);
+    let changedText = document.getElementById("changed").value.split(/\s+/);
     let differenceDiv = document.getElementById("difference");
     differenceDiv.innerHTML = "";
 
     for (let i = 0; i < changedText.length; i++) {
         let spanelement = document.createElement("span");
-        if (originalText[i] !== changedText[i]) {
+        if (originalText[i].toLowerCase() !== changedText[i].toLowerCase()) {
             spanelement.classList.add("changed");
         }
         spanelement.textContent = changedText[i] + " ";
